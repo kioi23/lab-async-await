037622940e2dfe095b6d65f245c1beb29c193b8b
kioi23 <kioijgithinji23@gmail.com>
2026-04-15 14:56:51 +0300

    complete codes

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here!
+//Fetch data using async/await
+
+async function getPosts() {
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await response.json();
+
+  displayPosts(posts);
+}
+
+// Display Posts
+function displayPosts(posts) {
+
+  const postList = document.getElementById("post-list");
+
+  for (let post of posts) {
+
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    postList.appendChild(li);
+  }
+}
+
+//calling getPosts()
+
+getPosts();
